029226d8f70692d49f01b4ba34c357eb7c67a5f1
Jet-Green <grishadzyin@gmail.com>
2025-01-26 10:47:06 +0500

    Добавьте файлы проекта.

---
+++ GeoCoordinates.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Math;
+
+namespace lab_9
+{
+    internal class GeoCoordinates
+    {
+        double latitude; // широта
+        double longitude; // долгота
+
+        public static int objCount = 0;
+
+        static double R = 6371;
+
+        public double Latitude
+        {
+            get => latitude;
+
+            set
+            {
+                if (value > 90 || value < -90)
+                {
+                    throw new Exception("Широта должна находиться в пределах от -90 до 90");
+                }
+                latitude = value;
+            }
+        }
+
+        public double Longitude
+        {
+            get => longitude;
+
+            set
+            {
+                if (value < -180 || value > 180)
+                {
+                    throw new Exception("Долгота должна находиться в пределах от -180 до 180");
+                }
+                longitude = value;
+            }
+        }
+
+        public GeoCoordinates()
+        {
+            Longitude = 0;
+            Latitude = 0;
+            objCount++;
+        }
+
+        public GeoCoordinates(double latitude, double longitude)
+        {
+            Latitude = latitude;
+            Longitude = longitude;
+            objCount++;
+        }
+        public GeoCoordinates(GeoCoordinates obj)
+        {
+            Latitude = obj.latitude;
+            Longitude = obj.longitude;
+            objCount++;
+        }
+
+        public double GetDistance(double lat2, double lon2)
+        {
+            double lat = (lat2 - Latitude) * (PI / 180);
+            double lon = (lon2 - Longitude) * (PI / 180);
+            double rez = Sin(lat / 2) * Sin(lat / 2) + Cos(Latitude * (PI / 180)) * Cos(lat2 * (PI / 180)) * Sin(lon / 2) * Sin(lon / 2);
+            double d = 2 * R * Atan2(Sqrt(rez), Sqrt(1 - rez));
+
+            return d;
+        }
+
+        public static double GetDistance(GeoCoordinates c1, GeoCoordinates c2)
+        {
+            double lat = (c2.Latitude - c1.Latitude) * (PI / 180);
+            double lon = (c2.Longitude - c1.Longitude) * (PI / 180);
+            double rez = Sin(lat / 2) * Sin(lat / 2) + Cos(c1.Latitude * (PI / 180)) * Cos(c2.Latitude * (PI / 180)) * Sin(lon / 2) * Sin(lon / 2);
+            double d = 2 * R * Atan2(Sqrt(rez), Sqrt(1 - rez));
+
+            return d;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine($"Широта: {Latitude}, долгота: {Longitude}");
+        }
+
+        public static GeoCoordinates operator ++ (GeoCoordinates c)
+        {
+            c.Latitude += 0.01;
+            c.Longitude += 0.01;
+
+            return c;
+        }
+
+        // противоположное значение
+        public static GeoCoordinates operator -(GeoCoordinates c)
+        {
+            c.Latitude = -c.Latitude;
+            c.Longitude = -c.Longitude;
+
+            return c;
+        }
+        // расположена ли на экваторе; явный
+        public static explicit operator bool(GeoCoordinates c)
+        {
+            if (c.Latitude == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+        // неявный
+        public static implicit operator string(GeoCoordinates c)
+        {
+            if (c.Longitude == 0)
+            {
+                return "Нулевой меридиан";
+            }
+            else if (c.Latitude > 0)
+            {
+                return "Восточная долгота";
+            }
+            else if (c.Longitude < 0)
+            {
+                return "Западная долгота";
+            }
+            return "";
+        }
+
+        // если на одной параллели
+        public static bool operator == (GeoCoordinates c1, GeoCoordinates c2)
+        {
+            if (c1.Longitude == c2.Longitude)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        // true, если на разных меридианах
+        public static bool operator != (GeoCoordinates c1, GeoCoordinates c2)
+        {
+            if (c1.Latitude != c2.Latitude) {
+                return true;
+            }
+            return false;
+        }
+
+        public override bool Equals(object obj) {
+            var objToCompare = obj as GeoCoordinates;
+            if (this.Longitude == objToCompare.Longitude && this.Latitude == objToCompare.Latitude)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,210 @@
+﻿
+
+namespace lab_9
+{
+    internal class Money
+    {
+        // private by default
+        int rub;
+        int kop;
+
+        static int count;
+
+        public static int GetCount
+        {
+            get => count;
+        }
+
+        // инкапсуляция через свойство
+        public int Rub
+        {
+            get
+            {
+                return rub;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    rub = 0;
+                    Console.WriteLine("Error!");
+                }
+                else
+                {
+                    rub = value;
+                }
+            }
+        }
+
+        public int Kop
+        {
+            get => kop;
+            set
+            {
+                if (value < 0)
+                {
+                    kop = 0;
+                    Console.WriteLine("Error!");
+                }
+                else if (value > 99)
+                {
+                    Rub += value / 100;
+                    kop = value % 100;
+                }
+                else
+                {
+                    kop = value;
+                }
+            }
+        }
+        // минимум 2, без параметров и с параметрами
+        public Money()
+        {
+            Rub = 0;
+            Kop = 0;
+            count++;
+        }
+
+        public Money(int rub, int kop)
+        {
+            Rub = rub;
+            Kop = kop;
+            count++;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine($"{Rub} руб., {Kop} коп.");
+        }
+
+        public void AddKop(int sum)
+        {
+            Kop = Kop + sum;
+        }
+
+        public static void AddKop(Money m, int sum)
+        {
+            m.Kop = m.Kop + sum;
+        }
+
+        // перегрузка унарных операторов
+        public static Money operator ++(Money m)
+        {
+            m.Kop += 1;
+            return m;
+        }
+
+        public static Money operator +(Money m, int sum)
+        {
+            Money temp = new Money(m.Rub, m.Kop);
+
+            temp.Kop += sum;
+            return temp;
+        }
+        // два варианта бинарного оператора для разного расположения.
+        public static Money operator +(int sum, Money m)
+        {
+            Money temp = new Money(m.Rub, m.Kop);
+
+            temp.Kop += sum;
+            return temp;
+        }
+
+        public static Money operator +(Money m1, Money m2)
+        {
+            Money temp = new Money();
+
+            temp.Kop = m1.Kop + m2.Kop;
+            temp.Rub = m1.Rub + m2.Rub;
+
+            return temp;
+        }
+
+        // неявное преобразование типа
+        public static implicit operator double(Money m)
+        {
+            double temp = m.Rub + m.Kop / 100.0;
+
+            return temp;
+        }
+
+        public static explicit operator int(Money m)
+        {
+            return m.Rub;
+        }
+
+        public static bool operator == (Money m1, Money m2)
+        {
+            return m1.Rub == m2.Rub && m1.Kop == m2.Kop;
+        }
+
+        public static bool operator != (Money m1, Money m2)
+        {
+            return !(m1.Rub != m2.Rub && m1.Kop != m2.Kop);
+        }
+    }
+
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Задание №1");
+            try
+            {
+                GeoCoordinates c0 = new GeoCoordinates(-1000, 20);
+            }
+            catch (Exception e) {
+                Console.WriteLine(e.Message);
+            }
+
+            try
+            {
+                GeoCoordinates c0 = new GeoCoordinates(20.123, -200);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.WriteLine();
+
+            GeoCoordinates c1 = new GeoCoordinates(57.973190, 56.168477);
+            c1.Show();
+            GeoCoordinates c2 = new GeoCoordinates(57.727685, 54.765219);
+            c2.Show();
+
+            Console.WriteLine($"Кол-во созданных объектов: {GeoCoordinates.objCount}");
+
+            Console.WriteLine(c1.GetDistance(57.727685, 54.765219));
+            Console.WriteLine(GeoCoordinates.GetDistance(c1, c2));
+
+            Console.WriteLine();
+            Console.WriteLine("Задание №2");
+
+            GeoCoordinates c3 = new GeoCoordinates(23, 44);
+            c3++;
+            c3.Show();
+
+            GeoCoordinates c4 = -c3;
+
+            c4.Show();
+
+            bool a = (bool) c4;
+            Console.WriteLine(a);
+            bool b = (bool) new GeoCoordinates(0, 12);
+            Console.WriteLine(b);
+
+            Console.WriteLine(c3);
+
+            GeoCoordinates c5 = new GeoCoordinates(44, 2.55);
+            GeoCoordinates c6 = new GeoCoordinates(44, -56.55);
+
+            Console.WriteLine(c5 == c6);
+
+            GeoCoordinates c7 = new GeoCoordinates(-67.66, 71);
+            GeoCoordinates c8 = new GeoCoordinates(-67.66, 91);
+        }
+    }
+}
